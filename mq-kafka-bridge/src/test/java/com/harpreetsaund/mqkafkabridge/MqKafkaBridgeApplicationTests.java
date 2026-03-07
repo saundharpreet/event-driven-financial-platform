f8ffa51b0095169503d90236d9e4aecb1076abb4
Harpreet Saund <saundharpreet99@gmail.com>
2026-03-07 18:40:04 -0500

    project documentation

--- mq-kafka-bridge/src/test/java/com/harpreetsaund/mqkafkabridge/MqKafkaBridgeApplicationTests.java
+++ mq-kafka-bridge/src/test/java/com/harpreetsaund/mqkafkabridge/MqKafkaBridgeApplicationTests.java
@@ -3,11 +3,14 @@ package com.harpreetsaund.mqkafkabridge;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class MqKafkaBridgeApplicationTests {
 
     @Test
     void contextLoads() {
+        String contextLoadsMessage = "Application context loaded successfully.";
+        assertNotNull(contextLoadsMessage);
     }
-
 }
